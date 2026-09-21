d15b15f6a3748e88aed1ea42ea7d771a6f98988e
Dopesonic <sasha_shlyakhtin@mail.ru>
2026-09-21 10:43:27 +0500

    TASK-1004: Удалил лишнее поле из Go-структуры и лишние Bun-теги, переименовал пакеты внутри Go-файлов

--- internal/app/entity/category.go
+++ internal/app/entity/category.go
@@ -11,8 +11,8 @@ type Category struct {
 	bun.BaseModel `bun:"table:category"`
 
 	ID        int64     `bun:"id,autoincrement"`
-	GUID      uuid.UUID `bun:"guid,pk,type:uuid"`
-	Name      string    `bun:"name,unique,notnull"`
+	GUID      uuid.UUID `bun:"guid,pk"`
+	Name      string    `bun:"name"`
 	CreatedAt time.Time `bun:"created_at"`
 	UpdatedAt time.Time `bun:"updated_at"`
 }
--- internal/app/repository/product/postgres.go
+++ internal/app/repository/product/postgres.go
@@ -1,4 +1,4 @@
-package category
+package pproduct
 
 import (
 	"context"
